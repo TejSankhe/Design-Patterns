fa4a4e78ff9ad25595d1ed599a3ed6a6555121a2
RajNSankhe <sankhe.r@husky.neu.edu>
2019-01-13 18:07:34 -0500

    Abstract Factory Pattern

---
+++ AbstractFactory/src/main/Blue.java
@@ -0,0 +1,8 @@
+package main;
+public class Blue implements Color {
+
+	   @Override
+	   public void fill() {
+	      System.out.println("Inside Blue::fill() method.");
+	   }
+	}
---
+++ AbstractFactory/src/main/Color.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface Color {
+	  void fill();
+}
